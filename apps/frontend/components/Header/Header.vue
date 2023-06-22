d59f31360ddf1e62a505baf8f81aaa98f7befac8
b_tsaryk <b.tsaryk@keepsolid.com>
2023-06-22 14:00:27 +0300

    Implement logout

--- apps/frontend/components/Header/Header.vue
+++ apps/frontend/components/Header/Header.vue
@@ -1,6 +1,16 @@
 <script lang="ts" setup>
-const burderMenuOpened = ref(false)
+const burgerMenuOpened = ref(false)
 const profileMenuOpened = ref(false)
+
+const { $api } = useNuxtApp()
+
+async function logout() {
+  await $api('/authentication/logout', {
+    method: 'POST',
+  })
+
+  window.location.href = '/'
+}
 </script>
 
 <template>
@@ -14,14 +24,14 @@ const profileMenuOpened = ref(false)
           </span>
         </NuxtLink>
 
-        <button class="inline-flex items-center justify-center p-2 text-gray-400 md:hidden focus:text-black hover:text-black focus:outline-none" @click="burderMenuOpened = !burderMenuOpened">
+        <button class="inline-flex items-center justify-center p-2 text-gray-400 md:hidden focus:text-black hover:text-black focus:outline-none" @click="burgerMenuOpened = !burgerMenuOpened">
           <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
-            <path :class="{ 'hidden': burderMenuOpened, 'inline-flex': !burderMenuOpened }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-            <path :class="{ 'hidden': !burderMenuOpened, 'inline-flex': burderMenuOpened }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+            <path :class="{ 'hidden': burgerMenuOpened, 'inline-flex': !burgerMenuOpened }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            <path :class="{ 'hidden': !burgerMenuOpened, 'inline-flex': burgerMenuOpened }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
           </svg>
         </button>
       </div>
-      <nav :class="{ flex: burderMenuOpened, hidden: !burderMenuOpened }" class="flex-grow flex-col items-center md:flex md:flex-row md:justify-end md:pb-0">
+      <nav :class="{ flex: burgerMenuOpened, hidden: !burgerMenuOpened }" class="flex-grow flex-col items-center md:flex md:flex-row md:justify-end md:pb-0">
         <a class="px-2 py-2 text-sm text-gray-500 lg:ml-auto lg:px-6 md:px-3 hover:text-blue-600" href="#">
           Dashboard
         </a>
@@ -52,7 +62,7 @@ const profileMenuOpened = ref(false)
                 Settings
               </a>
 
-              <a id="user-menu-item-2" href="#" class="block px-4 py-2 text-sm text-gray-500" role="menuitem" tabindex="-1">
+              <a id="user-menu-item-2" href="#" class="block px-4 py-2 text-sm text-gray-500" role="menuitem" tabindex="-1" @click.prevent="logout">
                 Sign out
               </a>
             </div>
